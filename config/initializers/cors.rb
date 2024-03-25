78058c500a8a1d122ea408012e22519aec655a4b
Yoko Harada <yokolet@gmail.com>
2024-03-25 17:26:59 +0900

    updates cors configuration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,18 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000, http://www.localhost:3000"
-
+    origins 'http://localhost:3000'
     resource "*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: true
+  end
+
+  allow do
+    origins 'http://www.localhost:3000'
+    resource "*",
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
              credentials: true
   end
 end
