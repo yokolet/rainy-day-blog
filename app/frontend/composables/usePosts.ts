90d34988201f2ef2171c097bf35a4ad157d19338
Yoko Harada <yokolet@gmail.com>
2024-04-09 14:21:22 +0900

    organizes graphql query related usages

--- app/frontend/composables/usePosts.ts
+++ app/frontend/composables/usePosts.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@vue/apollo-composable';
 import { POSTS_QUERY } from '../graphql/queries';
 import { computed, ComputedRef } from 'vue';
 
-interface PostsItem {
+export interface PostsItem {
   id: number;
   title: string;
   excerpt: string;
@@ -11,7 +11,11 @@ interface PostsItem {
   identifier: string;
 }
 
-export const usePosts = (): Array<PostsItem> => {
-  const { result } = useQuery(POSTS_QUERY);
-  return result.value ?? []
+export const usePosts = () => {
+  const { result, loading, error } = useQuery(POSTS_QUERY);
+  return {
+    result,
+    loading,
+    error
+  }
 }
--- app/frontend/views/PostsList.vue
+++ app/frontend/views/PostsList.vue
@@ -1,38 +1,40 @@
 <script setup lang="ts">
-import { useQuery } from '@vue/apollo-composable';
-import { POSTS_QUERY } from '../graphql/queries';
-import { computed } from 'vue';
-
-const { result } = useQuery(POSTS_QUERY);
-const posts  = computed(() => result.value ?? [])
-
-console.log(posts)
+import { PostsItem, usePosts } from '../composables/usePosts';
+import { ComputedRef } from 'vue';
+const { result, loading, error } = usePosts();
 </script>
 
 <template>
   <div class="grid">
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table class="table table-auto text-md sm:text-sm w-full">
-        <tbody>
-        <tr class="hover" v-for="post of posts['posts']" :key="post.id">
-          <td class="font-bold">{{ post.title }}</td>
-          <td>{{ post.excerpt }}...</td>
-          <td>{{ post.identifier }}</td>
-          <td><font-awesome-icon :icon="['far', 'comments']" /> {{ post.commentCount }}</td>
-          <td>{{ post.updatedAt }}</td>
-          <td>
-            <button>
-              <font-awesome-icon :icon="['fas', 'circle-chevron-right']" size="lg" />
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+      <div v-if="loading">Loading...</div>
+      <div v-else-if="error">Error {{ error.message }}</div>
+      <div v-else-if="result && result.posts">
+        <table class="table table-auto text-md sm:text-sm w-full">
+          <tbody>
+          <tr class="hover" v-for="post of result.posts" :key="post.id">
+            <td class="font-bold">{{ post.title }}</td>
+            <td>{{ post.excerpt }}...</td>
+            <td>{{ post.identifier }}</td>
+            <td><font-awesome-icon :icon="['far', 'comments']" /> {{ post.commentCount }}</td>
+            <td>{{ post.updatedAt }}</td>
+            <td>
+              <router-link :to="`/post/${post.id}`">
+                <button>
+                  <font-awesome-icon :icon="['fas', 'circle-chevron-right']" size="lg" />
+                </button>
+              </router-link>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+
     </div>
-    <div class="relative sm:py-2.5">
+    <div class="flex sm:py-2.5 justify-center">
       <div class="join">
-        <button class="join-item btn xs:btn-xs">1</button>
-        <button class="join-item btn xs:btn-xs btn-active">2</button>
+        <button class="join-item btn xs:btn-xs btn-active">1</button>
+        <button class="join-item btn xs:btn-xs">2</button>
         <button class="join-item btn xs:btn-xs">3</button>
         <button class="join-item btn xs:btn-xs">4</button>
       </div>
