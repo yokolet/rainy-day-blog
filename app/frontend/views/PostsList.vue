24a9bfac2dd57baa3d476862d3516618a78b886c
Yoko Harada <yokolet@gmail.com>
2024-04-15 22:32:55 +0900

    creates a post create form

--- app/frontend/views/PostsList.vue
+++ app/frontend/views/PostsList.vue
@@ -39,6 +39,48 @@ const { result, loading, error } = usePosts();
         <button class="join-item btn xs:btn-xs">4</button>
       </div>
     </div>
+    <div class="flex justify-end bottom-14 w-full">
+      <button class="text-indigo-700" onclick="post_form.showModal()">
+        <font-awesome-icon :icon="['fas', 'circle-plus']" size="3x" />
+      </button>
+    </div>
+    <!-- Open the modal using ID.showModal() method -->
+    <dialog id="post_form" class="modal">
+      <div class="modal-box w-11/12 max-w-5xl">
+        <form method="dialog">
+          <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+            <font-awesome-icon :icon="['fas', 'circle-xmark']" size="xl"/>
+          </button>
+        </form>
+        <form class="mt-4 p-4">
+          <label class="form-control">
+            <div class="label">
+              <span class="label-text">Title</span>
+            </div>
+            <input
+                type="text"
+                class="input input-bordered"
+                placeholder="Awesome title"
+                minlength="1"
+                maxlength="20" />
+          </label>
+          <label class="form-control">
+            <div class="label">
+              <span class="label-text">Content (max 1000 characters)</span>
+            </div>
+            <textarea
+                class="textarea textarea-bordered h-24"
+                placeholder="Write something awesome"
+                minlength="1"
+                maxlength="1000"></textarea>
+          </label>
+          <div class="modal-action">
+            <button class="btn btn-accent btn-outline btn-xs sm:btn-sm">Cancel</button>
+            <button class="btn btn-secondary btn-outline btn-xs sm:btn-sm">Create</button>
+          </div>
+        </form>
+      </div>
+    </dialog>
   </div>
 </template>
 
